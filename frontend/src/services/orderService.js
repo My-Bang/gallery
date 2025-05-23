5dd42dd4604c6d38a32081f592fce7c21cc439ef
My-Bang <smb7000@naver.com>
2025-05-23 12:17:07 +0900

    commit

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-// 주문 삽입
+// 주문 페이지에서 활용할 주문 서비스 코드 입니다.
+// 해당 서비스는 백엔드 API를 호출해서 주문 데이터를 처리하는 기능을 제공하고, 주문 입력 페이지에서도 활용됩니다.
+
 export const addOrder = (args) => {
     return axios.post("/v1/api/orders", args).catch(e => e.response);
 };
